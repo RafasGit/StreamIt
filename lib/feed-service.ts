818e7d2024901326918a47620730427912e69726
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-25 16:48:38 +0300

    streams

---
+++ lib/feed-service.ts
@@ -0,0 +1,63 @@
+import { db } from "./db";
+import { getSelf } from "./auth-service";
+
+export async function getStreams() {
+    let userId
+
+    try {
+		const self = await getSelf()
+		userId = self.id
+	} catch {
+		userId = null
+	}
+	let streams = []
+	if (userId) {
+		streams = await db.stream.findMany({
+			where: {
+				user: {
+					NOT: {
+						blocking: {
+							some: {
+								blockedId: userId
+							}
+						}
+					}
+				}
+			},
+			select: {
+				id: true,
+				user: true,
+				isLive: true,
+				name: true,
+				thumbnailUrl: true
+			},
+			orderBy: [
+				{
+					isLive: 'desc'
+				},
+				{
+					updatedAt: 'desc'
+				}
+			]
+		})
+	} else {
+		streams = await db.stream.findMany({
+			select: {
+				id: true,
+				user: true,
+				isLive: true,
+				name: true,
+				thumbnailUrl: true
+			},
+			orderBy: [
+				{
+					isLive: 'desc'
+				},
+				{
+					updatedAt: 'desc'
+				}
+			]
+		})
+	}
+	return streams
+}
