229cc7f7a2764930235579ad250c8947b4051cfd
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-26 17:48:29 +0300

    search results

--- lib/search-service.ts
+++ lib/search-service.ts
@@ -37,7 +37,23 @@ export async function getSearch(term?: string) {
 						}
 					}
 				]
-                
+                },
+			select: {
+				user: true,
+				id: true,
+				name: true,
+				isLive: true,
+				thumbnailUrl: true,
+				updatedAt: true
+			},
+			orderBy: [
+				{
+					isLive: 'desc'
+				},
+				{
+					updatedAt: 'desc'
+				}
+			]
             })
         } else {
             streams = await db.stream.findMany({
