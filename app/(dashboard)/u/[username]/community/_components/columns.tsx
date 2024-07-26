862e5ef49a5071d82f8665a4ce29f740ac70c8e8
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-26 18:47:43 +0300

    follower admin

---
+++ app/(dashboard)/u/[username]/community/_components/columns.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { ArrowUpDown } from 'lucide-react'
+import { ColumnDef } from '@tanstack/react-table'
+import { Button } from '@/components/ui/button'
+import { UserAvatar } from '@/components/user-avatar'
+import { UnblockButton } from './unblock-button'
