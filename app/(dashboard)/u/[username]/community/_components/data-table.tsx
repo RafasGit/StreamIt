62b8d4c3934b01eb9a76fb81b916e19777509def
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-26 18:43:41 +0300

    follower admin

---
+++ app/(dashboard)/u/[username]/community/_components/data-table.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import * as React from 'react'
+import {
+	ColumnDef,
+	ColumnFiltersState,
+	SortingState,
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable
+} from '@tanstack/react-table'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
