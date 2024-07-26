7b5de789fb0b143a6bf22efe39769ba9cf6e6a8e
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-26 19:12:12 +0300

    follower admin

--- app/(dashboard)/u/[username]/community/_components/columns.tsx
+++ app/(dashboard)/u/[username]/community/_components/columns.tsx
@@ -4,7 +4,7 @@ import { ArrowUpDown } from 'lucide-react'
 import { ColumnDef } from '@tanstack/react-table'
 import { Button } from '@/components/ui/button'
 import { UserAvatar } from '@/components/user-avatar'
-import { UnblockButton } from './unblock-button'
+import { UnblockButton } from './unblocking-button'
 
 export type BlockedUser = {
 	id: string
