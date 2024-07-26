fd353136f7e60b72462353a5b534e10d2da37faa
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-26 17:02:03 +0300

    feed recom

--- app/(browse)/search/_components/result-card.tsx
+++ app/(browse)/search/_components/result-card.tsx
@@ -28,7 +28,20 @@ export function ResultCard({ data }: ResultCardProps) {
 						username={data.user.username}
 					/>
 				</div>
-				
+				<div className='space-y-1'>
+					<div className='flex items-center gap-x-2'>
+						<p className='font-bold text-lg cursor-pointer hover:text-blue-500'>
+							{data.user.username}
+						</p>
+						<VerifiedMark />
+					</div>
+					<p className='text-sm text-muted-foreground'>{data.name}</p>
+					<p className='text-sm text-muted-foreground'>
+						{formatDistanceToNow(new Date(data.updatedAt), {
+							addSuffix: true
+						})}
+					</p>
+				</div>
 			</div>
 		</Link>
 	)
