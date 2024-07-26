e70456e026e3cf4014de0b01b5f6efc02ecbfdfd
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-26 17:06:30 +0300

    search results

--- lib/search-service.ts
+++ lib/search-service.ts
@@ -1,2 +1,25 @@
 import { db } from '@/lib/db'
 import { getSelf } from '@/lib/auth-service'
+
+
+export async function getSearch(term?: string) {
+	let userId
+	try {
+		const self = await getSelf()
+		userId = self.id
+	} catch {
+		userId = null
+	}
+	let streams = []
+	if (userId) {
+		streams = await db.stream.findMany({
+			where: {
+				user: {
+					NOT: {
+						blocking: {
+							some: {
+								blockedId: userId
+							}
+						}
+					}
+				},
