76823b46fd18041992e4025df9b37b70926f2cc0
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-29 08:00:44 +0300

    depoyment

--- app/(auth)/_components/logo.tsx
+++ app/(auth)/_components/logo.tsx
@@ -13,7 +13,7 @@ export const Logo = () => {
     <div className="flex flex-col items-center gap-y-4">
       <div className="bg-white rounded-full p-1">
         <Image
-          src="/spooky.svg"
+          src="/logo.svg"
           alt="StreamIt"
           height="80"
           width="80"
