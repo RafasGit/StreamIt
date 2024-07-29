ef8f2a08799d11a39af3a99c8eeeaac86973df11
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-29 08:22:38 +0300

    depoyment

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -16,7 +16,7 @@ export const Logo = () => {
       <div className='bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink'>
         <Image
           src='/logo.svg'
-          alt='Stream'
+          alt='StreamI'
           height='32'
           width='32'
         />
