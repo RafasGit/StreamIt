3b25202f7fe72634ca9612476c9241395c618ba4
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-29 08:10:59 +0300

    depoyment

--- app/(dashboard)/u/[username]/_components/navbar/logo.tsx
+++ app/(dashboard)/u/[username]/_components/navbar/logo.tsx
@@ -14,8 +14,8 @@ export function Logo() {
 			<div className='flex items-center gap-x-4 hover:opacity-75 transition'>
 				<div className='bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink'>
 					<Image
-						src='/spooky.svg'
-						alt='Gamehub'
+						src='/logo.svg'
+						alt=''
 						height='32'
 						width='32'
 					/>
